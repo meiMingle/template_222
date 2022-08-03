b9c0d3242cb3c0da3bd3b3fd9cea332828e1fc68
GitHub Action <action@github.com>
2022-08-03 05:01:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/meimingle/template222/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/meimingle/template222/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.meimingle.template222.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.meimingle.template222.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/meimingle/template222/MyPluginTest.kt
+++ src/test/kotlin/com/github/meimingle/template222/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.meimingle.template222
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
